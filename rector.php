e770045d047a90159d7a3398bcbb5a05ca73aee4
Arne De Smedt <arnedesmdt@gmail.com>
2023-02-21 11:28:31 +0100

    Add correct directories to rector config

--- rector.php
+++ rector.php
@@ -8,7 +8,12 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     // get parameters
-    $rectorConfig->paths([__DIR__ . '/src']);
+    $rectorConfig->paths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    );
     $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
 };

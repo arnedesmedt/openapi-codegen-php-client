d2bbc192c38580676e0441a78a1a6b8f45e2e4be
Arne De Smedt <arnedesmdt@gmail.com>
2023-02-22 14:34:52 +0100

    Run rector after generation

--- resources/dev/openapi-codegen-php-client.sh
+++ resources/dev/openapi-codegen-php-client.sh
@@ -41,6 +41,7 @@ docker run --rm -v "${rootdir}":/local ${generatorimage} generate -g openapi-cod
 cd "${rootdir}" && sudo chown -R "$(id -u):$(id -g)" Client.php README.md composer.json grumphp.yml phpcs.xml phpmd.xml phpstan.neon rector.php Model Endpoint Factory
 # Exit code of phpcbf is 1 if all is fixed https://github.com/squizlabs/PHP_CodeSniffer/issues/2898
 cd "${rootdir}" && (vendor/bin/phpcbf --extensions=php --report=full ./Client.php Model/ Endpoint/ Factory/ || true)
+cd "${rootdir}" && vendor/bin/rector
 
 if [ -x "${rootdir}/resources/scripts/after_run.sh" ]
 then

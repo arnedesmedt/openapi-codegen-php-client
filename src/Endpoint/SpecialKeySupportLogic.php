0808f70388667ef8ad74c392e6ff745ef427972c
Arne De Smedt <arnedesmdt@gmail.com>
2023-02-22 14:16:29 +0100

    Use special key suppport logic in endpoints

--- src/Endpoint/SpecialKeySupportLogic.php
+++ src/Endpoint/SpecialKeySupportLogic.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace ADS\OpenApi\Codegen\Endpoint;
 
+use function array_flip;
+
 trait SpecialKeySupportLogic
 {
-    /** @param array<string, string> $map */
-    private function convertByMap(string $key, array $map): string
+    /** @return array<string, string> */
+    abstract protected function keyMapping(): array;
+
+    public function convertKeyForRecord(string $key): string
     {
-        return $map[$key] ?? $key;
+        return $this->keyMapping()[$key] ?? $key;
+    }
+
+    public function convertKeyForArray(string $key): string
+    {
+        return array_flip($this->keyMapping())[$key] ?? $key;
     }
 }

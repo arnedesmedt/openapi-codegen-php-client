458067f642c32e78b2352cdfb64c53e4af010fb5
Arne De Smedt <arnedesmdt@gmail.com>
2023-03-08 16:33:13 +0100

    Add phpcbf and chown for all new files

--- resources/dev/openapi-codegen-php-client.sh
+++ resources/dev/openapi-codegen-php-client.sh
@@ -48,9 +48,9 @@ docker run --rm -v "${rootdir}":/local ${generatorimage} generate -g openapi-cod
                                                                --global-property=models \
                                                                --skip-overwrite
 
-cd "${rootdir}" && sudo chown -R "$(id -u):$(id -g)" Client.php ClientMock.php README.md composer.json grumphp.yml phpcs.xml phpmd.xml phpstan.neon rector.php Model Endpoint Factory Default
+cd "${rootdir}" && sudo chown -R "$(id -u):$(id -g)" Client.php ClientMock.php ApiClientMock.php ClientInterface.php README.md composer.json grumphp.yml phpcs.xml phpmd.xml phpstan.neon rector.php Model Endpoint Factory Default
 # Exit code of phpcbf is 1 if all is fixed https://github.com/squizlabs/PHP_CodeSniffer/issues/2898
-cd "${rootdir}" && (vendor/bin/phpcbf --extensions=php --report=full ./Client.php Model/ Endpoint/ Factory/ Default/ || true)
+cd "${rootdir}" && (vendor/bin/phpcbf --extensions=php --report=full ./Client.php ./ClientMock.php ./ApiClientMock.php ./ClientInterface.php Model/ Endpoint/ Factory/ Default/ || true)
 cd "${rootdir}" && vendor/bin/rector
 
 if [ -x "${rootdir}/resources/scripts/after_run.sh" ]

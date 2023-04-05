0fb184ca9707cf03f0c81bd18acef349a1857614
Arne De Smedt <arnedesmdt@gmail.com>
2023-04-05 16:52:21 +0200

    Update

--- resources/dev/fetch-openapi-and-generate.sh
+++ resources/dev/fetch-openapi-and-generate.sh
@@ -9,11 +9,11 @@ if [ -z $CI_COMMIT_REF_NAME ]; then
     echo "Environment variable CI_COMMIT_REF_NAME is not set."; exit 1;
 fi
 
-composer update
+composer install
 git checkout -B client-generation
 wget --no-check-certificate -cq $OPEN_API_SPEC_URL -O - | jq --indent 4 '.' > resources/api/api-spec.yml
 chmod -R 777 resources/api
-./openapi-codegen-php-client.sh
+vendor/bin/openapi-codegen-php-client.sh
 git status --porcelain
 if [ -z "$(git status --porcelain)" ]; then exit 0; fi
 git stash apply

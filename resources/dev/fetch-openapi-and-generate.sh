895caec6e004e97e7c115f2486514526b51fefe2
Arne De Smedt <arnedesmdt@gmail.com>
2023-04-05 16:55:58 +0200

    Do composer update instead of install

--- resources/dev/fetch-openapi-and-generate.sh
+++ resources/dev/fetch-openapi-and-generate.sh
@@ -9,7 +9,7 @@ if [ -z $CI_COMMIT_REF_NAME ]; then
     echo "Environment variable CI_COMMIT_REF_NAME is not set."; exit 1;
 fi
 
-composer install
+composer update
 git checkout -B client-generation
 wget --no-check-certificate -cq $OPEN_API_SPEC_URL -O - | jq --indent 4 '.' > resources/api/api-spec.yml
 chmod -R 777 resources/api

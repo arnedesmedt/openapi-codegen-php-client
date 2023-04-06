e34688d84e3a32579faaa9b7302381cd844a49f7
Arne De Smedt <arnedesmdt@gmail.com>
2023-04-06 11:02:01 +0200

    Use correct branch name

--- resources/dev/fetch-openapi-and-generate.sh
+++ resources/dev/fetch-openapi-and-generate.sh
@@ -10,14 +10,13 @@ if [ -z $CI_COMMIT_REF_NAME ]; then
 fi
 
 composer update
-git checkout -B client-generation
+git checkout -B feature/client-generation
 wget --no-check-certificate -cq $OPEN_API_SPEC_URL -O - | jq --indent 4 '.' > resources/api/api-spec.yml
 chmod -R 777 resources/api
 vendor/bin/openapi-codegen-php-client.sh
 git stash -- resources/api/api-spec.yml
-git status --porcelain
-if [ -z "$(git status --porcelain)" ]; then exit 0; fi
+if [ -z "$(git status --porcelain)" ]; then echo "No changes found during a new generate."; exit 0; fi
 git stash apply
 git add .
 git commit -m "Automatic update client by CI"
-git push origin client-generation:$CI_COMMIT_REF_NAME --force
+git push origin feature/client-generation:$CI_COMMIT_REF_NAME --force

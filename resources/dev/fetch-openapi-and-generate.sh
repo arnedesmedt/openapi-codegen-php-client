e95d22187bdc6f291c6222793407fce5b945e2d4
Arne De Smedt <arnedesmdt@gmail.com>
2023-04-06 14:20:00 +0200

    Remove test stuff

--- resources/dev/fetch-openapi-and-generate.sh
+++ resources/dev/fetch-openapi-and-generate.sh
@@ -9,8 +9,6 @@ if [ -z $CI_COMMIT_REF_NAME ]; then
     echo "Environment variable CI_COMMIT_REF_NAME is not set."; exit 1;
 fi
 
-git remote -v
-env
 git checkout -B feature/client-generation
 wget --no-check-certificate -cq $OPEN_API_SPEC_URL -O - | jq --indent 4 '.' > resources/api/api-spec.yml
 chmod -R 777 resources/api

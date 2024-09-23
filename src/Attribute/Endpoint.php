686ef01aa0bd3baa67656a2ecb51337997c24edc
Arne De Smedt <arnedesmdt@gmail.com>
2024-09-23 15:59:14 +0200

    Add endpoint attribute
    We want to use it to map the endpoint with the client method

---
+++ src/Attribute/Endpoint.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\OpenApi\Codegen\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Endpoint
+{
+    public function __construct(
+        private readonly string $endpointClass,
+    ) {
+    }
+
+    public function endpointClass(): string
+    {
+        return $this->endpointClass;
+    }
+}
